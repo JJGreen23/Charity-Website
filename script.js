e090893317c3edea4abbb5255d65448464c7c815
Joseph Green <greenjj26@yahoo.com>
2021-06-02 20:43:15 -0600

    Working-searchbar-api-added

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-$("#formSearchBar").submit(function (e) {
-    e.preventDefault()
+$("#form").submit(function (e) {
+    e.preventDefault();
 
-    var query = $("#searchBar").val()
+    var query = $("#searchBar").val();
 
     var API_KEY1 = '8ff98b40c11551781da7775b1f612864'
 
+    var result = '';
+
     var url = 'http://api.serpstack.com/search?access_key=' + API_KEY1 + '&type=web&query=' + query
+
+    console.log(url)
 
     $.get(url, function (data){
 
         $("#searchResults").html('')
 
-        console.log(data)
+        console.log(data);
 
         data.organic_results.forEach(res => {
             
             result = `
             
-                <h4>${res.title}</h4><br><a target="_blank" href"${res.url}">${res.url}</a>
-                <p>${res.snippet}</p>
+                <h4>${res.title}</h4><a href="${res.url}">${res.url}</a>
             
             `
             $("#searchResults").append(result)
@@ -26,4 +29,5 @@ $("#formSearchBar").submit(function (e) {
         });
 
     })
-})
+});
+
